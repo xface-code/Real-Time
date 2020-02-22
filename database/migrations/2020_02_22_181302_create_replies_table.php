b476bcd85fc80006e254f970d3666dd56ada89ec
unknown <the_casawi@live.fr>
2020-02-22 19:33:09 +0100

    Create Model, Migration Controller and Factories

--- database/migrations/2020_02_22_181302_create_replies_table.php
+++ database/migrations/2020_02_22_181302_create_replies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateRepliesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('replies', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
+            $table->text('body');
+
+            $table->integer('question_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+
+            $table->foreign('question_id')->references('id')->on('question')->ondelete('cascade');
+
+
             $table->timestamps();
         });
     }
@@ -31,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('replies');
     }
 }
